63050c16fdd29141a37c33344ad4e5ac75b1aa83
Josselin Morvan <morvan.josselin@gmail.com>
2026-01-15 11:36:37 +0100

    added pimpmytowsty

--- src/TowSty.jl
+++ src/TowSty.jl
@@ -33,11 +33,35 @@ function __init__()
   global CSL_PATH = joinpath(ASSETS_PATH, "static/csl/style.csl")
 end
 
+"""
+    pimpmytowsty(filename::String)
+
+Load a user-defined file from the project directory if it exists,
+otherwise load the default package file.
+
+"""
+function pimpmytowsty(filename::String)
+  projectfile = joinpath(PROJECT_PATH, filename)
+  packagefile = joinpath(@__DIR__, filename)
+  
+  if isfile(projectfile)
+    @info "Loading user-defined $filename from project directory"
+    include(projectfile)
+  elseif isfile(packagefile)
+    include(packagefile)
+  else
+    @warn "File $filename not found in project or package directory"
+  end
+end
+
 include("utils.jl")
 include("apistylo.jl")
 include("data.jl")
-include("content.jl")
-include("webapp.jl")
+
+# Load content.jl and webapp.jl from project if available
+pimpmytowsty("content.jl")
+pimpmytowsty("webapp.jl")
+
 include("static.jl")
 
 export meta, corpuses, articles, workspace
