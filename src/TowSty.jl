d2ad71b5e69b6d5c73eebf8774babb19a555e01d
Josselin Morvan <morvan.josselin@gmail.com>
2026-01-16 16:18:33 +0100

    reviewed pimpmytowsty with init

--- src/TowSty.jl
+++ src/TowSty.jl
@@ -11,52 +11,38 @@ using YAML
 using Slugify
 using Unicode
 
-# Initialize paths at module load time (not in __init__)
-const PROJECT_PATH = pwd()
-pkgdir = @__DIR__
-@info "Project dir : $PROJECT_PATH"
-@info "Package dir : $pkgdir"
-const ASSETS_PATH = joinpath(PROJECT_PATH, "assets")
-const TEMP_PATH = joinpath(PROJECT_PATH, "temp")
-const TEMPLATES_PATH = joinpath(PROJECT_PATH, "templates")
-const DATA_PATH = joinpath(PROJECT_PATH, "content", "workspace.json")
-const BIB_PATH = joinpath(TEMP_PATH, "bib.bib")  # Bibliography files are created on the fly
-const CSL_PATH = joinpath(ASSETS_PATH, "static/csl/style.csl")  # fichier csl / citations bibliographiques
-
-"""
-    pimpmytowsty(filename::String)
-
-Load a user-defined file from the project directory if it exists,
-otherwise load the default package file.
-
-"""
-function pimpmytowsty(filename::String)
-  projectfile = joinpath(PROJECT_PATH, filename)
-  packagefile = joinpath(@__DIR__, filename)
-  
-  if isfile(projectfile)
-    @info "Loading user-defined $filename from project directory"
-    include(projectfile)
-  elseif isfile(packagefile)
-    include(packagefile)
-  else
-    @warn "File $filename not found in project or package directory"
-  end
-end
-
-include("utils.jl")
-include("apistylo.jl")
-include("data.jl")
-include("rendering.jl")
-
-# Load content.jl and webapp.jl from project if available
 function __init__()
-  pimpmytowsty("content.jl")
-  pimpmytowsty("webapp.jl")
+  const PROJECT_PATH = pwd()
+  pkgdir = @__DIR__
+  @info "Project dir : $PROJECT_PATH"
+  @info "Package dir : $pkgdir"
+  const ASSETS_PATH = joinpath(PROJECT_PATH, "assets")
+  const TEMP_PATH = joinpath(PROJECT_PATH, "temp")
+  const TEMPLATES_PATH = joinpath(PROJECT_PATH, "templates")
+  const DATA_PATH = joinpath(PROJECT_PATH, "content", "workspace.json")
+  const BIB_PATH = joinpath(TEMP_PATH, "bib.bib")  # Bibliography files are created on the fly
+  const CSL_PATH = joinpath(ASSETS_PATH, "static/csl/style.csl")  # fichier csl / citations bibliographiques
+
+  include("utils.jl")
+  include("apistylo.jl")
+  include("data.jl")
+  include("rendering.jl")
+
+  if isfile(joinpath(PROJECT_PATH, "content.jl"))
+    include(joinpath(PROJECT_PATH, "content.jl"))
+  else
+    include("content.jl")
+  end
+
+  if isfile(joinpath(PROJECT_PATH, "webapp.jl"))
+    include(joinpath(PROJECT_PATH, "webapp.jl"))
+  else
+    include("webapp.jl")
+  end
+
+  include("static.jl")
+
+  export meta, corpuses, articles, workspace
 end
-
-include("static.jl")
-
-export meta, corpuses, articles, workspace
 
 end
