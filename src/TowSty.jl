b0d2e40efa43f2e5adf3371d45c4e3f75384619c
Josselin Morvan <morvan.josselin@gmail.com>
2026-01-19 12:00:51 +0100

    path

--- src/TowSty.jl
+++ src/TowSty.jl
@@ -12,15 +12,14 @@ using YAML
 using Slugify
 using Unicode
 
-const PROJECT_PATH = pwd()
+PROJECT_PATH = pwd()
 @info "Project is at $(PROJECT_PATH)"
-
-const ASSETS_PATH = joinpath(PROJECT_PATH, "assets")
-const TEMP_PATH = joinpath(PROJECT_PATH, "temp")
-const TEMPLATES_PATH = joinpath(PROJECT_PATH, "templates")
-const DATA_PATH = joinpath(PROJECT_PATH, "content", "workspace.json")
-const BIB_PATH = joinpath(TEMP_PATH, "bib.bib")  # Bibliography files are created on the fly
-const CSL_PATH = joinpath(ASSETS_PATH, "static/csl/style.csl")  # fichier csl / citations bibliographiques
+ASSETS_PATH = joinpath(PROJECT_PATH, "assets")
+TEMP_PATH = joinpath(PROJECT_PATH, "temp")
+TEMPLATES_PATH = joinpath(PROJECT_PATH, "templates")
+DATA_PATH = joinpath(PROJECT_PATH, "content", "workspace.json")
+BIB_PATH = joinpath(TEMP_PATH, "bib.bib")  # Bibliography files are created on the fly
+CSL_PATH = joinpath(ASSETS_PATH, "static/csl/style.csl")  # fichier csl / citations bibliographiques
 
 include("utils.jl")
 include("apistylo.jl")
