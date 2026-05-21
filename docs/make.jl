6a2978e2f356c95f1167a2d456047af1e8caeebe
Josselin Morvan <morvan.josselin@gmail.com>
2026-05-21 17:12:54 +0200

    reviewed docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,17 @@
 using Documenter, TowSty
 
 makedocs(;
-  format = Documenter.HTML(;
-    prettyurls = get(ENV, "CI", nothing) == "true"
-  ),
-  authors = "J. Morvan <josselin.morvan@univ-rouen.fr>, \nJ. Dehut <julien.dehut@univ-rouen.fr>",
-  pages = [
-    "TowSty" => "index.md",
-    "Getting started" => "gettingstarted.md"
-  ],
-  sitename = "Guide - TowSty",
-  repo = "https://gitlab.huma-num.fr/ceen/towsty/towsty.jl"
-  #remotes=nothing
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", nothing) == "true"
+    ),
+    authors = "J. Morvan <morvan.josselin@gmail.com>",
+    pages = [
+        "TowSty.jl" => "index.md",
+    ],
+    sitename = "TowSty.jl"
 )
 
 
+deploydocs(
+    repo = "github.com/juliadh/TowSty.jl.git",
+)
